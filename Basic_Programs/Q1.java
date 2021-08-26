b37e5844bdc8e16feb4ec56f2316af5e6b33e344
Pranav Karande <pranav.3943@gmail.com>
2021-08-26 16:55:50 +0530

    Completed Q1

--- Basic_Programs/Q1.java
+++ Basic_Programs/Q1.java
@@ -1,10 +1,22 @@
 /**
  * Q1. Read the radius and print the area of a circle.
  */
+import java.util.Scanner;
+// Importing Math library for constant PI
+import java.lang.Math;
 
-class Q1{
+public class Q1{
     public static void main(String args[]) {
         // Read the radius
+        System.out.print("Enter radius of circle: ");
+        Scanner radius_scanner = new Scanner(System.in);
+        double radius = radius_scanner.nextDouble();
+        radius_scanner.close();
 
+        // Calculate area
+        double area = Math.PI * radius * radius;
+
+        // Print area
+        System.out.println("Area of circle with radius " + radius + " is " + area + ".");
     }
 }
